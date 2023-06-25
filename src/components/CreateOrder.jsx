2e3a607df8184d73cd19e4e71a20eb76d4b89ace
Shubham Tandon <iamshubham316@gmail.com>
2023-06-25 18:42:18 +0530

    toggle wash type and dynamic prices work

--- src/components/CreateOrder.jsx
+++ src/components/CreateOrder.jsx
@@ -3,6 +3,7 @@ import Navbar from './Navbar'
 import SideNav from './SideNav'
 import '../css/CreateOrder.css'
 
+//images
 import jeans from '../assets/img/products/jeans.svg'
 import shirt from '../assets/img/products/shirt.svg'
 import tshirt from '../assets/img/products/tshirt.svg'
@@ -17,30 +18,66 @@ import sTowel from '../assets/img/wash-btn/selected/towel.svg'
 import uTowel from '../assets/img/wash-btn/unselected/towel.svg'
 import sBleach from '../assets/img/wash-btn/selected/bleach.svg'
 import uBleach from '../assets/img/wash-btn/unselected/bleach.svg'
+//images-end
 
 import { useEffect, useState } from 'react'
 
 export default function() {
   const [products, setProducts] = useState([])
   const [imgArr, setImgArr] = useState([jeans,shirt,tshirt,trouser,boxers,jogger])
-  const [statusWash, setStatus] = useState([false,false,false,false])
-  const [selectedWash, setSelected] = useState([sWash,sIron,sTowel,sBleach])
-  const [unSelectedWash, setUnSelected] = useState([uWash.uIron,uTowel,uBleach])
+
+  const [quantArr, setQuant] = useState([0,0,0,0,0,0])
+  const [isWash, setWash] = useState([false,false,false,false,false,false]);
+  const [isIron, setIron] = useState([false,false,false,false,false,false]);
+  const [isTowel, setTowel] = useState([false,false,false,false,false,false]);
+  const [isBleach, setBleach] = useState([false,false,false,false,false,false]);
+
+  const [addOnPrice, setAddOn] = useState([0,0,0,0,0,0])
+  
 
   useEffect(()=>{
     fetch("http://localhost:5000/products")
     .then(res=>res.json())
-    .then(data=>setProducts(data))
-
+    .then(data=>{
+      setProducts(data)
+    })
+    
   },[])
-  console.log(products);
+  // console.log(products);
 
   function ToggleWash(idx){
-    setStatus([
-      ...statusWash.slice(0,idx),
-      !statusWash[idx],
-      ...statusWash.slice(idx+1,statusWash.length)
+    setWash([
+      ...isWash.slice(0,idx),
+      !isWash[idx],
+      ...isWash.slice(idx+1,6)
     ])
+    
+  }
+  function ToggleIron(idx){
+    setIron([
+      ...isIron.slice(0,idx),
+      !isIron[idx],
+      ...isIron.slice(idx+1,6)
+    ])
+    
+  }
+  function ToggleBleach(idx){
+    setBleach([
+      ...isBleach.slice(0,idx),
+      !isBleach[idx],
+      ...isBleach.slice(idx+1,6)
+    ])
+    
+  }
+  function ToggleTowel(idx){
+    setTowel([
+      ...isTowel.slice(0,idx),
+      !isTowel[idx],
+      ...isTowel.slice(idx+1,6)
+    ])
+  }
+  function AddItem(id){
+
   }
 
   return (
@@ -49,21 +86,63 @@ export default function() {
       <SideNav />
       <div id="create-order-main">
         <p id="create-title">Create Order</p>
-        <p id="create-description"></p>
+        <p id="create-description">You can order a laundry service by selecting the quantity of each type of clothing and the type of wash desired.</p>
         <div id="create-order-area">
           {products.map((item)=>{
             return <div id="item-row">
               <img className='item-img' src={imgArr[item.iconurl]} alt='icon'/>
-              <p id="item-name">{item.productName}</p>
-              <p id="item-description">{item.description}</p>
-              <input type="number" id="item-quant" />
+              <div id="name-desc">
+                <p id="item-name">{item.productName}</p>
+                <p id="item-description">{item.description}</p>
+              </div>
+              
+              
+              <input type="number" onChange={(e)=>setQuant([...quantArr.slice(0,item.iconurl),e.target.value,...quantArr.slice(item.iconurl+1,6)])} id="item-quant" />
+
               <span id="wash-btns">
-                {statusWash.map((e,idx)=>{
-                  return e==false ? <img onClick={(idx)=> ToggleWash(idx)} src={unSelectedWash[idx]} />: <img onClick={(idx)=> ToggleWash(idx)} src={selectedWash[idx]} />
-                })}
+                {!isWash[item.iconurl] ?
+                  <img src={uWash} onClick={()=>{
+                    ToggleWash(item.iconurl)
+                    setAddOn([...addOnPrice.slice(0,item.iconurl),addOnPrice[item.iconurl]+item.addOn[0].price,...addOnPrice.slice(item.iconurl+1,6)])
+                  }}/> :
+                  <img src={sWash} onClick={()=>{
+                    ToggleWash(item.iconurl)
+                    setAddOn([...addOnPrice.slice(0,item.iconurl),addOnPrice[item.iconurl]-item.addOn[0].price,...addOnPrice.slice(item.iconurl+1,6)])
+                  }}/>
+                }
+                {!isIron[item.iconurl] ?
+                  <img src={uIron} onClick={()=>{
+                    ToggleIron(item.iconurl)
+                    setAddOn([...addOnPrice.slice(0,item.iconurl),addOnPrice[item.iconurl]+item.addOn[2].price,...addOnPrice.slice(item.iconurl+1,6)])
+                  }}/> :
+                  <img src={sIron} onClick={()=>{
+                    ToggleIron(item.iconurl)
+                    setAddOn([...addOnPrice.slice(0,item.iconurl),addOnPrice[item.iconurl]-item.addOn[2].price,...addOnPrice.slice(item.iconurl+1,6)])
+                  }}/>
+                }
+                {!isBleach[item.iconurl] ?
+                  <img src={uBleach} onClick={()=> {
+                    ToggleBleach(item.iconurl)
+                    setAddOn([...addOnPrice.slice(0,item.iconurl),addOnPrice[item.iconurl]+item.addOn[1].price,...addOnPrice.slice(item.iconurl+1,6)])
+                  }}/> :
+                  <img src={sBleach} onClick={()=> {
+                    ToggleBleach(item.iconurl)
+                    setAddOn([...addOnPrice.slice(0,item.iconurl),addOnPrice[item.iconurl]-item.addOn[1].price,...addOnPrice.slice(item.iconurl+1,6)])
+                  }}/>
+                }
+                {!isTowel[item.iconurl] ?
+                  <img src={uTowel} onClick={()=> {
+                    ToggleTowel(item.iconurl)
+                    setAddOn([...addOnPrice.slice(0,item.iconurl),addOnPrice[item.iconurl]+item.addOn[3].price,...addOnPrice.slice(item.iconurl+1,6)])
+                  }}/> :
+                  <img src={sTowel} onClick={()=> {
+                    ToggleTowel(item.iconurl)
+                    setAddOn([...addOnPrice.slice(0,item.iconurl),addOnPrice[item.iconurl]-item.addOn[3].price,...addOnPrice.slice(item.iconurl+1,6)])
+                  }}/>
+                }
               </span>
-              <p id="item-price">{item.price}</p>
-              <button id="add-item-btn">Add</button>
+              <p id="item-price">Rs. {item.price+addOnPrice[item.iconurl]}</p>
+              <button onClick={()=>AddItem(item.iconurl)} id="add-item-btn">Add</button>
             </div>
           })}
         </div>
