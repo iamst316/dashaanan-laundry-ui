f94be7d2b4c9c389c9e43d7f20b8c8fc06de24cd
Shubham Tandon <iamshubham316@gmail.com>
2023-07-08 20:19:10 +0530

    working on admin forms + css

--- src/components/AddProduct.jsx
+++ src/components/AddProduct.jsx
@@ -1,5 +1,16 @@
-
+import '../css/AddProduct.css'
 
 export default function(){
-    
+    return (
+        <div id="add-p-main">
+            <input type="text" placeholder="Product Name" />
+            <input type="text" placeholder="Product Description" />
+            <input type="number" placeholder="Enter Price of the Item" />
+            <input type="number" placeholder="Enter Price of Deep Washing" />
+            <input type="number" placeholder="Enter Price of Ironing" />
+            <input type="number" placeholder="Enter Price of Bleaching" />
+            <input type="number" placeholder="Enter Price of Crisp Folding" />
+            <button>Add Product</button>
+        </div>
+    )
 }
--- src/components/AddStore.jsx
+++ src/components/AddStore.jsx
@@ -1,5 +1,13 @@
-
+import '../css/AddStore.css';
 
 export default function(){
-    
+    return(
+        <div id="add-s-main">
+            <input type="text" placeholder="Store Name" />
+            <input type="text" placeholder="Store Description" />
+            <input type="number" placeholder="Enter Price of the Item" />
+            
+            <button>Add Store</button>
+        </div>
+    )
 }
