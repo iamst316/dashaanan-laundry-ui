b03b9b39d78a833d4bbf678faad4741ce9fec7a7
Shubham Tandon <iamshubham316@gmail.com>
2023-07-09 20:00:35 +0530

    cont. work on admin forms

--- src/components/AddProduct.jsx
+++ src/components/AddProduct.jsx
@@ -1,16 +1,66 @@
 import '../css/AddProduct.css'
-
+import { useNavigate } from 'react-router-dom'
+import { useState,useEffect } from 'react'
 export default function(){
+    const navigate = useNavigate()
+    const [newItem, setItem]= useState({})
+    const [wash,setWash] = useState(0)
+    const [iron,setIron] = useState(0)
+    const [bleach,setBleach] = useState(0)
+    const [towel,setTowel] = useState(0)
+    useEffect(()=>{
+    },[newItem])
+
+    function AddItem(){
+        // e.preventDefault()
+        newItem.addOn = [
+            {
+                type: 'wash',
+                price: wash,
+                status: false
+            },
+            {
+                type: 'iron',
+                price: iron,
+                status: false
+            },
+            {
+                type: 'bleach',
+                price: bleach,
+                status: false
+            },
+            {
+                type: 'towel',
+                price: towel,
+                status: false
+            },
+        ]
+        const requestOptions = {
+            method: 'POST',
+            // headers: { 'Content-Type': 'application/json','Authorization': "Bearer " + auth, },
+            body: JSON.stringify(newItem)
+        };
+        fetch('http://localhost:4000/add-store', requestOptions)
+            .then(response => {
+                response.json()
+                console.log(response)
+            })
+
+        navigate("/admin-home")
+    }
+
     return (
         <div id="add-p-main">
-            <input type="text" placeholder="Product Name" />
-            <input type="text" placeholder="Product Description" />
-            <input type="number" placeholder="Enter Price of the Item" />
-            <input type="number" placeholder="Enter Price of Deep Washing" />
-            <input type="number" placeholder="Enter Price of Ironing" />
-            <input type="number" placeholder="Enter Price of Bleaching" />
-            <input type="number" placeholder="Enter Price of Crisp Folding" />
-            <button>Add Product</button>
+            <input type="text" placeholder="Product Name" onChange={(e)=>newItem.productName=e.target.value} />
+            <input type="text" placeholder="Product Description" onChange={(e)=>newItem.description=e.target.value} />
+            <input type="number" placeholder="Enter Price of the Item" onChange={(e)=>newItem.price=e.target.value} />
+
+            <input type="number" placeholder="Enter Price of Deep Washing" onChange={(e)=>setWash(e.target.value)} />
+            <input type="number" placeholder="Enter Price of Ironing" onChange={(e)=>setIron(e.target.value)} />
+            <input type="number" placeholder="Enter Price of Bleaching" onChange={(e)=>setBleach(e.target.value)} />
+            <input type="number" placeholder="Enter Price of Crisp Folding" onChange={(e)=>setTowel(e.target.value)} />
+
+            <button onClick={()=>AddItem()}>Add Product</button>
         </div>
     )
 }
--- src/components/AddStore.jsx
+++ src/components/AddStore.jsx
@@ -1,13 +1,35 @@
+import { useEffect, useState } from 'react';
 import '../css/AddStore.css';
+import { useNavigate } from 'react-router-dom';
 
-export default function(){
+export default function(){ 
+    const navigate = useNavigate()
+    const [newStore, setStore]= useState({})
+    useEffect(()=>{
+    },[newStore])
+    function AddStore(){
+        // e.preventDefault()
+        const requestOptions = {
+            method: 'POST',
+            // headers: { 'Content-Type': 'application/json','Authorization': "Bearer " + auth, },
+            body: JSON.stringify(newStore)
+        };
+        fetch('http://localhost:5000/add-store', requestOptions)
+            .then(response => {
+                response.json()
+                console.log(response)
+            })
+
+        navigate("/admin-home")
+    }
     return(
         <div id="add-s-main">
-            <input type="text" placeholder="Store Name" />
-            <input type="text" placeholder="Store Description" />
-            <input type="number" placeholder="Enter Price of the Item" />
-            
-            <button>Add Store</button>
+            <input type="text" placeholder="Store Name" onChange={(e)=>newStore.storeName=e.target.value} />
+            <input type="text" placeholder="Store Description" onChange={(e)=>newStore.label=e.target.value} />
+            <input type="text" placeholder="Store Address" onChange={(e)=>newStore.address=e.target.value} />
+            <input type="number" placeholder="Enter Store Contact Number" onChange={(e)=>newStore.telephone=e.target.value} />
+            <input type="number" placeholder="Enter Store Delivery Charges" onChange={(e)=>newStore.delivery_charges=e.target.value} />
+            <button onClick={()=>AddStore()}>Add Store</button>
         </div>
     )
 }
