c5e585d8a6dfdb3fc8a4eb2239054f7b2ed6416c
Shubham Tandon <iamshubham316@gmail.com>
2023-08-16 16:44:36 +0530

    successful add address

--- src/components/AddAddress.jsx
+++ src/components/AddAddress.jsx
@@ -5,20 +5,24 @@ import SideNav from "./SideNav";
 import '../css/AddAddress.css'
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
+import { setUser } from "../redux/slices/userSlice";
 import axios from 'axios';
 
 export default function(){
-    const navigate = useNavigate()
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    // const loggedInUser = useSelector((state) => state.user);
     const [addForm,setAddressForm] = useState({})
     function AddNew(e){
         e.preventDefault();
 
         const apiUrl = 'http://localhost:4000/add-address';
 
-        axios.post(apiUrl, addForm, { withCredentials: true })
-            // .then(response => {
-
-            // })
+        axios.patch(apiUrl, addForm, { withCredentials: true })
+            .then(response => {
+                // dispatch(setUser(User));
+                console.log("res--->",response)
+            })
             .catch(error => {
                 console.error('Error:', error.message);
             });
